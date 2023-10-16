f87a8cd2829a14c96c472b5c68a99732a5fdaca6
Ryan Wee <rw2800@columbia.edu>
2023-10-15 22:45:38 -0600

    Remove extraneous files

--- scripts/disable_HWPF.sh
+++
@@ -1,5 +0,0 @@
-## You should be present in $HW_PF
-sudo ./wrmsr -a 0x1a4 15
-
-##disables on all cores
-
--- scripts/enable_HWPF.sh
+++
@@ -1,3 +0,0 @@
-## You should be present in $HW_PF
-sudo ./wrmsr -a 0x1a4 0 #enables on all cores
-
